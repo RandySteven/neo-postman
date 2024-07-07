3f045ee265e6088ee483d3398c2f8780e21dbf4f
Randy Steven <randysteven12@gmail.com>
2024-07-07 09:19:14 +0700

    create jira

--- usecases/jira_issue_usecase.go
+++ usecases/jira_issue_usecase.go
@@ -7,15 +7,34 @@ import (
 	"github.com/RandySteven/neo-postman/entities/payloads/responses"
 	repositories_interfaces "github.com/RandySteven/neo-postman/interfaces/repositories"
 	usecases_interfaces "github.com/RandySteven/neo-postman/interfaces/usecases"
-	"github.com/RandySteven/neo-postman/pkg/jira"
+	jira_client "github.com/RandySteven/neo-postman/pkg/jira"
+	"github.com/andygrunwald/go-jira"
+	"log"
 )
 
 type jiraIssueUsecase struct {
 	jiraRepository repositories_interfaces.JiraIssueRepository
-	jiraApiAction  jira.JiraAction
+	jiraApiAction  jira_client.JiraAction
 }
 
 func (j *jiraIssueUsecase) CreateJiraTicket(ctx context.Context, request *requests.CreateJiraIssueRequest) (result *responses.CreateJiraIssueResponse, customErr *apperror.CustomError) {
+	issue := jira.Issue{
+		Fields: &jira.IssueFields{
+			Project: jira.Project{
+				Key: request.Project.Key,
+			},
+			Type: jira.IssueType{
+				Name: request.IssueType.Name.ToString(),
+			},
+			Summary: request.Summary,
+		},
+	}
+	response, err := j.jiraApiAction.CreateIssue(ctx, &issue)
+	if err != nil {
+		return nil, apperror.NewCustomError(apperror.ErrInternalServer, `failed to create jira`, errr)
+	}
+	log.Println(response)
+
 	return
 }
 
@@ -28,7 +47,7 @@ var _ usecases_interfaces.JiraIssueUseCase = &jiraIssueUsecase{}
 func NewJiraIssueUsecase(
 	jiraRepository repositories_interfaces.JiraIssueRepository,
 ) *jiraIssueUsecase {
-	jira, err := jira.NewJiraClient()
+	jira, err := jira_client.NewJiraClient()
 	if err != nil {
 		return nil
 	}
