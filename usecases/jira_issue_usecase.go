93e57ae3172d5a2b478682e51ce2e153f4d14299
Randy Steven <randysteven12@gmail.com>
2024-07-07 12:28:20 +0700

    jira usecase logic

--- usecases/jira_issue_usecase.go
+++ usecases/jira_issue_usecase.go
@@ -2,14 +2,16 @@ package usecases
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/RandySteven/neo-postman/apperror"
+	"github.com/RandySteven/neo-postman/entities/models"
 	"github.com/RandySteven/neo-postman/entities/payloads/requests"
 	"github.com/RandySteven/neo-postman/entities/payloads/responses"
 	repositories_interfaces "github.com/RandySteven/neo-postman/interfaces/repositories"
 	usecases_interfaces "github.com/RandySteven/neo-postman/interfaces/usecases"
 	jira_client "github.com/RandySteven/neo-postman/pkg/jira"
 	"github.com/andygrunwald/go-jira"
-	"log"
+	"sync"
 )
 
 type jiraIssueUsecase struct {
@@ -33,7 +35,55 @@ func (j *jiraIssueUsecase) CreateJiraTicket(ctx context.Context, request *reques
 	if err != nil {
 		return nil, apperror.NewCustomError(apperror.ErrInternalServer, `failed to create jira`, err)
 	}
-	log.Println(response)
+	jiraIssue := &models.JiraIssue{}
+
+	var (
+		wg    sync.WaitGroup
+		errCh = make(chan *apperror.CustomError)
+	)
+
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		err = json.Unmarshal(jiraIssue.Request, &issue)
+		if err != nil {
+			errCh <- apperror.NewCustomError(apperror.ErrInternalServer, `failed to create jira`, err)
+			return
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		err = json.Unmarshal(jiraIssue.Response, &response.Response.Body)
+		if err != nil {
+			errCh <- apperror.NewCustomError(apperror.ErrInternalServer, `failed to create jira`, err)
+			return
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		close(errCh)
+	}()
+	responseMap := make(map[string]interface{})
+
+	err = json.Unmarshal(jiraIssue.Response, &responseMap)
+	if err != nil {
+		return nil, apperror.NewCustomError(apperror.ErrInternalServer, `failed to create jira`, err)
+	}
+
+	jiraIssue.Link = responseMap["self"].(string)
+
+	jiraIssue, err = j.jiraRepository.Save(ctx, jiraIssue)
+	if err != nil {
+		return nil, apperror.NewCustomError(apperror.ErrInternalServer, `failed to create jira`, err)
+	}
+
+	select {
+	case customErr = <-errCh:
+		return nil, customErr
+	}
 
 	return
 }
