557272303ec1ea8bead4d811b012b83a32131738
Randy Steven <randysteven12@gmail.com>
2024-07-21 13:26:28 +0700

    success add cli to create file

--- cmd/neo_post/ruler/main.go
+++ cmd/neo_post/ruler/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os/exec"
 	"strings"
+	"sync"
 )
 
 func main() {
 	i := -1
 	file := ""
 	model := ""
+	repository := ""
 	for i != 0 {
 		menu()
 		fmt.Print(">>")
@@ -21,8 +24,68 @@ func main() {
 			fmt.Print("Insert model name : ")
 			fmt.Scanf("%s", &model)
 			file = fmt.Sprintf("%s.go", strings.ToLower(model))
-			cli := fmt.Sprintf("nano ./entities/models/%s", file)
-			exec.Command(cli)
+			modelStruct := strings.ToUpper(string(model[0])) + model[1:]
+			createNewStruct := fmt.Sprintf(`package models \n\ntype %s struct {}`, modelStruct)
+			cmd := exec.Command("sh", "-c", fmt.Sprintf("echo \"%s\" > ./entities/models/%s && clear", createNewStruct, file))
+
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println(out)
+		case 2:
+		//TODO
+		case 3:
+			repositoryIntrContent, err := ioutil.ReadFile("./files/txt/template/repository_interfaces.txt")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			repositoryContent, err := ioutil.ReadFile("./files/txt/template/repository.txt")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			repositoryStruct := string(repositoryContent)
+			repositoryIntrface := string(repositoryIntrContent)
+
+			fmt.Print("Insert repository name : ")
+			fmt.Scanf("%s", &repository)
+
+			modelName := strings.ToUpper(string(repository[0])) + repository[1:]
+			structName := strings.ToLower(string(repository[0])) + repository[1:] + "Repository"
+			interfaceName := strings.ToUpper(string(repository[0])) + repository[1:] + "Repository"
+
+			file = fmt.Sprintf("%s_repository.go", strings.ToLower(repository))
+
+			createIntrfaceCmd := fmt.Sprintf(repositoryIntrface, interfaceName, modelName)
+			createStrctCmd := fmt.Sprintf(repositoryStruct, structName, interfaceName, structName)
+
+			var wg sync.WaitGroup
+			wg.Add(2)
+			go func() {
+				defer wg.Done()
+				cmd := exec.Command("sh", "-c", fmt.Sprintf("echo \"%s\" > ./interfaces/repositories/%s && clear", createIntrfaceCmd, file))
+				_, err := cmd.CombinedOutput()
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			}()
+
+			go func() {
+				defer wg.Done()
+				cmd := exec.Command("sh", "-c", fmt.Sprintf("echo \"%s\" > ./repositories/%s && clear", createStrctCmd, file))
+				_, err := cmd.CombinedOutput()
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			}()
+
+			wg.Wait()
 		}
 	}
 }
