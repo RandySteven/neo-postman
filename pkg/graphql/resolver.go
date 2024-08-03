0beedaf3b2813c4fc71382e5ba88e2b3d15a9c31
Randy Steven <randysteven12@gmail.com>
2024-08-03 08:09:51 +0700

    try to implement

--- pkg/graphql/resolver.go
+++ pkg/graphql/resolver.go
@@ -1,7 +1,20 @@
 package graphql_pkg
 
-type Resolver struct {
-	TestDataResolver interface {
-		CreateTestData()
+import (
+	"context"
+	"github.com/RandySteven/neo-postman/apperror"
+	"github.com/RandySteven/neo-postman/entities/payloads/responses"
+	"github.com/graphql-go/graphql"
+)
+
+type (
+	ResolverRoot interface {
+		TestDataResolver() TestDataResolver
 	}
-}
+
+	TestDataResolver struct {
+		GetAllTestDatas func(ctx context.Context, obj interface{}, next graphql.FieldResolver) (res []*responses.TestRecordList, customErr *apperror.CustomError)
+	}
+
+	Resolver struct{}
+)
